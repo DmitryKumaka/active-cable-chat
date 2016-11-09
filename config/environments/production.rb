d7e767dc46575a7467f95444eda679a59912dc34
Dima <kumakad@gmail.com>
2016-11-09 20:26:34 +0200

    heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://chatit2.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://chatit2.herokuapp.com/', /http:\/\/chatit2.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://chatit2.herokuapp.com/', /https:\/\/chatit2.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
